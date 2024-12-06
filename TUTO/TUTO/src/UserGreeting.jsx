3f7bb3df68823aa86665a9b8f5cae58486329cd5
achourmohamedamine <163700221+achourmohamedamine@users.noreply.github.com>
2024-12-06 21:48:35 +0100

    Delete TUTO/TUTO directory

--- TUTO/TUTO/src/UserGreeting.jsx
+++
@@ -1,9 +0,0 @@
-
-function UserGteeting(props){
-
-    const Welcome= <h2>Welcome {props.name}</h2>;
-    const Login= <h2>Please log in</h2>;
-    return (props.isLoggedin ? Welcome : Login);
-
-}
-export default UserGteeting
